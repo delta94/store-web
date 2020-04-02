21ec642b9b9cb939bd93faa46d825352f1066b1d
Behuamuh <sven1980@list.ru>
2020-04-02 13:28:43 +0300

    Done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,13 +57,13 @@ export default MyApp;
 
 const GlobalStyle = createGlobalStyle`
   html, body {
-    margin: 0;
-	  padding: 0;
-	  border: 0;
+    border: 0;
     min-height: 100%;
   }
 
   * {
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
     font-family: Arial, sans-serif;
   }
