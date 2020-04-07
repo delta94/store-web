e424491998e0dd609ce4c93fe53436a96bf1d8ff
Behuamuh <sven1980@list.ru>
2020-04-07 09:32:34 +0300

    Added game cards for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { COLORS } from 'store-library';
+import { Grid, Container } from 'store-library';
 import { useQuery } from '@apollo/react-hooks';
 import { ALL_GAMES } from '~/api/queries';
 import GameCardContainer from '~/components/GameCardContainer';
@@ -9,23 +9,33 @@ interface Props {
   className?: string;
 }
 
+const cardTypes = ['xs', 's', 'm', 'l', 'xl'];
+
 const Home = (props: Props) => {
   const { className } = props;
   const { data } = useQuery(ALL_GAMES);
-  const games: any[] = data?.store?.games?.slice(0, 2) || [];
+  const games: any[] = data?.store?.games?.slice(0, 18) || [];
 
   return (
     <Wrapper className={className}>
-      {/* Тест для GameCardContainer */}
-      {games.map(({ id }) => (
-        <GameCardWrapper key={id} >
-          <GameCardContainer id={id} type="xl" />
-        </GameCardWrapper>
-      ))}
-      {/* Тест цветов и шрифтов */}
-      {Object.keys(COLORS).map(name => (
-        <StyledColor key={name} name={name}>{name}</StyledColor>
-      ))}
+      <Container>
+        {/* Тест для GameCardContainer */}
+        <Grid.Row gap="12px">
+          {games.map(({ id }, index) => (
+            <Grid.Col 
+              key={id}
+              xs={index < 4 ? 12 : 6}
+              sm={index < 4 ? 12 : index < 12 ? 3 : 12}
+              lg={index < 4 ? 6 : undefined}
+            >
+              <GameCardWrapper type={cardTypes[Math.floor((index / 4) % cardTypes.length)]}>
+                <GameCardContainer id={id} type={cardTypes[Math.floor((index / 4) % cardTypes.length)]} />
+              </GameCardWrapper>
+            </Grid.Col>
+          ))}
+        </Grid.Row>
+        
+      </Container>
     </Wrapper>
   );
 };
@@ -38,12 +48,15 @@ const Wrapper = styled.div`
   color: white;
 `;
 
-const StyledColor = styled.div<{ name: string }>`
-  height: 20px;
-  background-color: ${({ name }) => COLORS[name]};
-`;
+// const StyledColor = styled.div<{ name: string }>`
+//   height: 20px;
+//   background-color: ${({ name }) => COLORS[name]};
+// `;
 
-const GameCardWrapper = styled.div`
-  height: 200px;
-  margin: 16px;
+const GameCardWrapper = styled.div<{ type?: string }>`
+  height: ${({ type }) => {
+    if (type === 'xs') return 'unset';
+    if (type === 'xl' || type === 'l') return '400px';
+    return '200px';
+  }};
 `;
