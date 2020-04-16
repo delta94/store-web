63d00b99eaaeb608e0edc4053d88ee8865a789b6
ramenskiy <ramenskiy.m@gmail.com>
2020-04-16 21:13:55 +0300

    use PageModule on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Grid, Container, QUERIES, GameCardContainer } from 'store-library';
-import { useQuery } from '@apollo/react-hooks';
+import { Container, PageModule, screenSize } from 'store-library';
+
+import modules from '~/mainPageModules';
 
 interface Props {
   className?: string;
 }
 
-const cardTypes = ['xs', 's', 'm', 'l', 'xl'];
-
 const Home = (props: Props) => {
   const { className } = props;
-  const { data } = useQuery(QUERIES.ALL_GAMES);
-  const games: any[] = data?.store?.games?.slice(0, 18) || [];
 
   return (
     <Wrapper className={className}>
       <Container>
-        {/* Тест для GameCardContainer */}
-        <Grid.Row gap="12px">
-          {games.map(({ id }, index) => (
-            <Grid.Col 
-              key={id}
-              xs={index < 4 ? 12 : 6}
-              sm={index < 4 ? 12 : index < 12 ? 3 : 12}
-              lg={index < 4 ? 6 : undefined}
-            >
-              <GameCardWrapper type={cardTypes[Math.floor((index / 4) % cardTypes.length)]}>
-                <GameCardContainer id={id} type={cardTypes[Math.floor((index / 4) % cardTypes.length)]} />
-              </GameCardWrapper>
-            </Grid.Col>
-          ))}
-        </Grid.Row>
-        
+        <Content>
+          {modules.map(({ component, ...rest }) => {
+            const Component = PageModule[component];
+            return (
+              <React.Fragment key={component}>
+                <Component key={component} {...rest} />
+                <Devider />
+              </React.Fragment>
+            );
+          })}
+        </Content>
       </Container>
     </Wrapper>
   );
@@ -46,10 +38,18 @@ const Wrapper = styled.div`
   color: white;
 `;
 
-const GameCardWrapper = styled.div<{ type?: string }>`
-  height: ${({ type }) => {
-    if (type === 'xs') return 'unset';
-    if (type === 'xl' || type === 'l') return '400px';
-    return '200px';
-  }};
+const Content = styled.div`
+  padding-top: 30px;
+`;
+
+export const Devider = styled.div`
+  height: 40px;
+
+  @media only screen and (min-width: ${screenSize.TABLET}) {
+    height: 48px;
+  }
+
+  @media only screen and (min-width: ${screenSize.LAPTOP}) {
+    height: 50px;
+  }
 `;
