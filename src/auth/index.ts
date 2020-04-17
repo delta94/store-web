39cfa5e835bc6f9a8a7a1645e0609ba7fefcc2a7
ramenskiy <ramenskiy.m@gmail.com>
2020-04-17 15:24:50 +0300

    api url fix

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,10 +1,10 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.API_URL;
 
 const baseLoginUrl = `${API_URL}/v1/auth/login`;
 const baseLogoutUrl = `${API_URL}/v1/auth/logout`;
 
 export const login = () => {
-  if(typeof window === 'undefined') return;
+  if (typeof window === 'undefined') return;
 
   const redirectUrl = window.location.href;
   const loginUrl = `${baseLoginUrl}?redirect=${redirectUrl}`;
@@ -13,7 +13,7 @@ export const login = () => {
 };
 
 export const logout = () => {
-  if(typeof window === 'undefined') return;
+  if (typeof window === 'undefined') return;
 
   const redirectUrl = window.location.href;
   const logoutUrl = `${baseLogoutUrl}?redirect=${redirectUrl}`;
