a8429ab612f98315ac882cbd81429c5eef501dd2
Behuamuh <sven1980@list.ru>
2020-05-28 04:13:16 +0300

    added bot detection on gamepage

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
+import { Game as GameType } from 'store-library/src/types';
+import { apolloClient, GET_GAME } from 'store-library/src/api';
+import Game from '~/components/Game';
+import { detectBot } from '~/helpers';
 
-const GamePage = () => {
+interface Props {
+  game: GameType | null;
+}
+
+const GamePage = (props: Props) => {
+  const { game } = props;
   const router = useRouter();
   const slug = router.query.slug;
 
-  if (slug) router.push(`/game?slug=${slug}`, `/game/${slug}`);
+  useEffect(() => {
+    if (game) return;
 
-  return null;
+    if (!slug) {
+      router.push('/404');
+      return;
+    }
+
+    router.push(`/game?slug=${slug}`, `/game/${slug}`);
+  }, []);
+
+  if (!game) return null;
+
+  return <Game game={game} />;
 };
 
 export default React.memo(GamePage);
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const slug = context.params?.slug;
+  const userAgent = context.req.headers['user-agent'];
+  const isBot = detectBot(userAgent);
+
+  let game: GameType | null = null;
+
+  if (isBot) {
+    try {
+      const { data } = await apolloClient.query({
+        query: GET_GAME,
+        variables: { slug },
+      });
+
+      game = data?.store?.gameBySlug;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return { props: { game } };
+};
