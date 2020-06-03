d8d91097c77572c2c0b90a3e46c4130d8da0fe1e
Behuamuh <sven1980@list.ru>
2020-06-03 10:27:08 +0300

    changed pwa config

--- next.config.js
+++ next.config.js
@@ -15,9 +15,15 @@ module.exports = withPWA(withTM({
   pwa: {
     dest: 'public',
     disable: !isProd,
-    navigateFallback: '/index.html',
-    navigateFallbackDenylist: [
-      new RegExp('^/api'),
+    runtimeCaching: [
+      {
+        urlPattern: '/',
+        handler: 'networkFirst',
+      },
+      {
+        urlPattern: new RegExp('^/api'),
+        handler: 'networkOnly',
+      },
     ],
   },
   webpack: (config, options) => {
