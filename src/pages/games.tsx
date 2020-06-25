c282670216a2df4787ef6d9a4432efb23fca0f13
ramenskiy <ramenskiy.m@gmail.com>
2020-06-25 21:43:49 +0300

    add initialFilter from query

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -3,6 +3,8 @@ import { Catalog } from 'store-library';
 import SearchFilter from '~/components/SearchFilter';
 import apolloClient from '~/apolloClient';
 import { useRouter } from 'next/router';
+import { Filter } from 'store-library/src/components/Catalog/types';
+import { getUrlParameter } from '~/helpers';
 
 const GamesPage = () => {
   const [showFiltersModal, setShowFiltersModal] = useState(false);
@@ -25,10 +27,52 @@ const GamesPage = () => {
     );
   };
 
+  const handleChangeFilter = ({ sortType, platforms, features, genres, offset, limit }: Filter) => {
+    const query: Record<string, string | number> = {
+      sortType,
+      offset,
+      limit,
+    };
+
+    if (platforms.length) {
+      query.platforms = platforms.map(({ id }) => id).join(',');
+    }
+
+    if (features.length) {
+      query.features = features.map(({ id }) => id).join(',');
+    }
+
+    if (genres.length) {
+      query.genres = genres.map(({ id }) => id).join(',');
+    }
+
+    router.push(
+      '/games',
+      { query },
+    );
+  };
+
+  const genresParam = getUrlParameter('genres');
+  const featuresParam = getUrlParameter('features');
+  const platformsParam = getUrlParameter('platforms');
+  const limitParam = getUrlParameter('limit');
+  const offsetParam = getUrlParameter('offset');
+
+  const initialFilter = {
+    genres: genresParam?.length ? genresParam.split(',') : [],
+    features: featuresParam?.length ? featuresParam.split(',') : [],
+    platforms: platformsParam?.length ? platformsParam.split(',') : [],
+    limit: limitParam ? Number(limitParam) : 50,
+    offset: offsetParam ? Number(offsetParam) : 0,
+    sortType: getUrlParameter('sortType') || 'releaseDate',
+  };
+
   return (
     <>
       <SearchFilter onShowFiltersModal={handleShowFiltersModal} filterCount={filterCount} />
       <Catalog
+        initialFilter={initialFilter}
+        onChangeFilter={handleChangeFilter}
         onCardClick={handleCardClick}
         client={apolloClient}
         onChangeFilterCount={handleFilterCountChange}
