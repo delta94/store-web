5abfa63b6a7bc508d4080de14a1e7f80ce0bf122
ramenskiy <ramenskiy.m@gmail.com>
2020-06-30 10:42:19 +0300

    add qu & track game page view from search

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Game from '~/components/Game';
 import { GET_GAME } from 'store-library/src/api';
 import { useRouter } from 'next/router';
@@ -6,11 +6,21 @@ import { useQuery } from '@apollo/react-hooks';
 import { Game as GameType } from 'store-library/src/types';
 import PageLoading from '~/components/PageLoading';
 import SearchFilter from '~/components/SearchFilter';
+import { qu } from '~/helpers';
 
 const GamePage = () => {
   const router = useRouter();
-  const { slug } = router.query;
+  const { slug, fromSearch } = router.query;
   const { data, error, loading } = useQuery(GET_GAME, { variables: { slug } });
+
+  useEffect(() => {
+    if (fromSearch) {
+      qu('myevent', {
+        key: 'GAME_PAGE_VIEW_FROM_SEARCH',
+        data: { slug },
+      });
+    }
+  }, []);
 
   if (loading) return <PageLoading />;
 
